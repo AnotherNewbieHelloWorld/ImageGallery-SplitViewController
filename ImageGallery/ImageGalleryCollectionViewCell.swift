072d28722db1539daaa0e3f1bca21c418c9be748
AnotherNewbieHelloWorld <khor.alena16@gmail.com>
2020-04-22 10:20:49 +0500

    Image Gallery blanks

---
+++ ImageGallery/ImageGalleryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageGalleryCollectionViewCell.swift
+//  ImageGallery
+//
+//  Created by Apple User on 22.04.2020.
+//  Copyright © 2020 Alena Khoroshilova. All rights reserved.
+//
+
+import UIKit
+
+class ImageGalleryCollectionViewCell: UICollectionViewCell {
+    
+}
